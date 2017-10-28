23109bf4632fd7f7277771a8997c68d3d693f036
Maria Mata <mariajo.mata@gmail.com>
2017-10-28 12:13:31 -0600

    small updates to routes

--- routes.py
+++ routes.py
@@ -25,30 +25,35 @@ def analyze_tone():
 
 @app.route('/signup', methods=['POST'])
 def signup():
+    # need to add validation
     data = request.json
-    # if valid_signup(data['username'], data['email'], data['password']):
-    newuser = User(data['username'], data['email'], data['password'])
-    db.session.add(newuser)
-    db.session.commit()
-    response = {'status': 1, 'message': 'Success!'}
-    return jsonify(response)
-    # else:
-    #     result = {'status': 0, 'message': 'Error'}
-    #     return result
+    user = User.query.filter_by(username = data['username']).first()
+    email = User.query.filter_by(email = data['email']).first()
+    if user is not None or email is not None:
+        response = {'error': 'Username or email already in use.'}
+        return jsonify(response)
+    else:
+        newuser = User(data['username'], data['email'], data['password'])
+        db.session.add(newuser)
+        db.session.commit()
+        response = {'message': 'Success!'}
+        return jsonify(response)
 
 @app.route('/signin', methods=['POST'])
 def signin():
+    # need to add validation
     data = request.json
     user = User.query.filter_by(username = data['username']).first()
     if user is not None and user.check_password(data['password']):
-        response = {'status': 1, 'message': 'Success!'}
+        response = {'message': 'Success!'}
         return jsonify(response)
     else:
-        response = {'status': 0, 'message': 'Error'}
+        response = {'error': 'Incorrect username or password.'}
         return jsonify(response)
 
 @app.route('/images/<user_id>', methods=['GET', 'POST'])
 def images(user_id):
+    # need to add validation
     if request.method == 'GET':
         images = Image.query.filter_by(user_id = user_id)
         response = [image.serialize for image in images]
@@ -58,7 +63,7 @@ def images(user_id):
         newimage = Image(user_id, data['url'], data['name'], data['description'])
         db.session.add(newimage)
         db.session.commit()
-        response = {'status': 1, 'message': 'Success!'}
+        response = {'message': 'Success!'}
         return jsonify(response)
 
 
